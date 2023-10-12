23ba21959d6e2a117f27ea4d236f2fc7d5a8bb20
MatuHotu <matuhotu@gmail.com>
2023-10-12 20:55:02 +0200

    fixed file names

--- src/charts/ageChart.py
+++ src/charts/ageChart.py
@@ -1,13 +1,16 @@
 import pandas as pd
 
 import plotly.express as px
-
 import json
 
-import datetime
+from datetime import datetime
 from dash_bootstrap_templates import load_figure_template
 
 load_figure_template("slate")
+
+x = []
+y = []
+z = []
 
 f = open("../../data/losses.json")
 losses = json.load(f)
@@ -19,40 +22,17 @@ lastUpdateDate = metaData["last_update_date"]
 title = f"Rolling Average Model Age Of Lost Russian Tanks        Updated: " + lastUpdateDate
 f.close()
 
-lastRecordedDate = datetime.datetime.strptime(losses[0]["date"], "%d %b %Y")
-firstRecordedDate = datetime.datetime.strptime(losses[len(losses) - 1]["date"], "%d %b %Y")
-dates = list(pd.date_range(start=firstRecordedDate, end=lastRecordedDate))
+for loss in losses:
+    if loss["model_match"] != "No Match Found":
+        date = datetime.strptime(loss["date"], "%d %b %Y")
+        x.append(date)
+        y.append(int(loss["model_year"]))
+        z.append(loss["name"])
 
-data = {
-    "T-54/55": [0] * len(dates), 
-    "T-62": [0] * len(dates), 
-    "T-64": [0] * len(dates), 
-    "T-72": [0] * len(dates),
-    "T-80": [0] * len(dates),
-    "T-90": [0] * len(dates),
-    "T-14": [0] * len(dates),
-    "Date": [] 
-}
+df = pd.DataFrame({'Date': x, 'Model Year': y, 'Name': z})
 
-for idx, date in enumerate(dates):
-    data["Date"].append(date.date())
-
-    for loss in losses:
-        modelMatch = loss["model_match"]
-        losstime = datetime.datetime.strptime(loss["date"], "%d %b %Y")
-        if modelMatch != "No Match Found" and losstime < date:
-            data[modelMatch][idx] += 1
-
-
-formatedData = pd.melt(pd.DataFrame(data=data), id_vars="Date").sort_values(by=["Date", "value", "variable"])
-
-fig = px.bar(formatedData, x="value", y="variable", animation_frame="Date", title=f'Russian Tanks Lost By Model (Updated: {metaData["last_update_date"]})', text_auto=True, orientation='h')
-
+fig = px.scatter(df, x="Date", y="Model Year", trendline="rolling", trendline_options=dict(window=50), opacity=0.15, template = "slate", title=title, hover_data="Name")
 fig.update_layout(xaxis_title="", yaxis_title="")
 
-fig.layout.updatemenus[0].buttons[0].args[1]['frame']['duration'] = 100
-fig.layout.updatemenus[0].buttons[0].args[1]['transition']['duration'] = 0
-fig.update_layout(xaxis_range=[0, formatedData.max()["value"] + 50])
-
-with open("../../charts/age.html", 'w') as f:
+with open("../../charts/cumulative.html", 'w') as f:
     f.write(fig.to_html(include_plotlyjs='cdn'))
